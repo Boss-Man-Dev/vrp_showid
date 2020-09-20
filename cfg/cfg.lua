02f870c7e716136eea1bf46bca2bd6fe0e520e06
Boss_Man <54071671+Boss-Man-Dev@users.noreply.github.com>
2020-09-20 16:54:12 -0500

    Add files via upload

---
+++ cfg/cfg.lua
@@ -0,0 +1,26 @@
+local cfg = {}
+
+--HEX colors
+cfg.color = "color:#1014ff;"
+cfg.id_color = "color:#f100ff;"
+
+cfg.display_css = [[
+.div_name{
+  position: fixed;
+  bottom: 18%;
+  left: 18%;
+  font-size: 25px;
+  font-family: Pricedown;
+  color: white;
+}
+.div_id{
+  position: fixed;
+  bottom: 18%;
+  left: 20%;
+  font-size: 25px;
+  font-family: Pricedown;
+  color: white;
+}
+]]
+
+return cfg
---
+++ cfg/lang/en.lua
@@ -0,0 +1,9 @@
+
+local lang = {
+	id = {
+		name = "{1}",
+		my_id = "{1}",
+	},
+}
+
+return lang
